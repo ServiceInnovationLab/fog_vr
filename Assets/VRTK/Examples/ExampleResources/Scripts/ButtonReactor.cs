bdff0c7ee9da188cd8a3e8431049ac023b68bc9b
Brenda Wallace <shiny@cpan.org>
2018-04-11 14:24:24 +1200

    Adding VRTK

---
+++ Assets/VRTK/Examples/ExampleResources/Scripts/ButtonReactor.cs
@@ -0,0 +1,36 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEventHelper;
+
+    public class ButtonReactor : MonoBehaviour
+    {
+        public GameObject go;
+        public Transform dispenseLocation;
+
+        private VRTK_Button_UnityEvents buttonEvents;
+
+        private void Start()
+        {
+            buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+            if (buttonEvents == null)
+            {
+                buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+            }
+            buttonEvents.OnPushed.AddListener(handlePush);
+        }
+
+        private void handlePush(object sender, Control3DEventArgs e)
+        {
+
+            if (e.normalizedValue > .8f)
+            {
+                VRTK_Logger.Info("Pushed");
+                print("Button pushed");
+            }
+            //GameObject newGo = (GameObject)Instantiate(go, dispenseLocation.position, Quaternion.identity);
+            //Destroy(newGo, 10f);
+            
+        }
+    }
+}
